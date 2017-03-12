20bad7ae7499ab28ac36a5dda0881c84c40be9a2
Jeremy Woertink <jeremywoertink@gmail.com>
2017-03-12 10:40:38 -0700

    Need to use class level methods because instance throws weird errors

--- src/godaddy-api.cr
+++ src/godaddy-api.cr
@@ -15,7 +15,6 @@ module Godaddy
     end
 
     def initialize
-      @client = HTTP::Client.new(host: BASE_URL + "/#{API.configuration.version}", tls: true)
       @headers = HTTP::Headers.new
       @headers["Authorization"] = "sso-key #{API.configuration.api_key}:#{API.configuration.api_secret}"
       @headers["Content-Type"] = "application/json"
@@ -23,7 +22,7 @@ module Godaddy
 
     {% for method in ["get", "post", "put", "patch", "delete"] %}
       def {{method.id}}(path, payload = nil)
-        response = @client.{{method.id}}(path, headers: @headers, body: payload.to_json)
+        response = HTTP::Client.{{method.id}}("#{BASE_URL}/#{API.configuration.version}" + path, headers: @headers, body: payload.to_json)
         JSON.parse(response.body)
       end
     {% end %}
